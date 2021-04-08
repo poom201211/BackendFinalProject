2fcc11f6a65f60bd33561f1bdd39eb3d120efb95
poom201211 <thanapoom.ta@ku.th>
2021-04-08 23:46:30 +0700

    Added required fields and seperated by <br />\n<br /> instead

--- api/controllers/contentController.js
+++ api/controllers/contentController.js
@@ -5,18 +5,18 @@ exports.userContent = async (req,res) => {
         // kratooTitle: Title of kratoo
         // kratooDesc: Connected description from same user
         var allContent = await content.findOne({_id: req.query._id}).exec();
-        var descArray = []
+        var descArray = ""
         if(allContent._source.desc_full){
-            descArray.push(allContent._source.desc_full);
+            descArray += "<br />\n<br />" + (allContent._source.desc_full);
         }else{
-            descArray.push(allContent._source.desc);
+            descArray += "<br />\n<br />" +  (allContent._source.desc);
         }
         allContent._source.comments.forEach(comment => {
             if(comment.uid == allContent._source.uid){
-                descArray.push(comment.desc)
+                descArray += "<br />\n<br />" + (comment.desc)
             }
         });
-        var userContent = {kratooTitle: allContent._source.title, kratooDesc: descArray}
+        var userContent = {kratooID: allContent._id,kratooTitle: allContent._source.title, kratooDesc: descArray, owner: allContent._source.nickname, created_time: allContent._source.created_time}
         return res.status(200).json(userContent);
     } catch (error) {
         console.log(error)

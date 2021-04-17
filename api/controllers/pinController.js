807900bf1737683b48709b1039b6034cbe8ca3d3
Hayato <hokiz.1999@gmail.com>
2021-04-17 23:58:57 +0700

    Add min min lat lng

--- api/controllers/pinController.js
+++ api/controllers/pinController.js
@@ -1,8 +1,24 @@
 // import pin Model
 const Pin = require("../models/pinModel");
 
+const sortPin = (a, b) => {
+  if (a.postions.lat < b.postions.lat) {
+    return -1;
+  }
+  if (a.postions.lat > b.postions.lat) {
+    return 1;
+  }
+  if (a.postions.lng < b.postions.lng) {
+    return -1;
+  }
+  if (a.postions.lng > b.postions.lng) {
+    return 1;
+  }
+  return 0;
+};
+
 exports.types = (req, res) => {
-  var type = req.query.type;
+  const { type, maxLat, maxLng, minLat, minLng } = req.query;
 
   const typeList = ["cafe", "attraction", "restaurant", "all"];
 
@@ -14,7 +30,35 @@ exports.types = (req, res) => {
       if (err) {
         return res.status(500).json({ message: err.message });
       } else {
-        return res.status(200).json(pin);
+        console.log(pin);
+        const inZone = pin.filter(
+          (p) =>
+            p.postions.lat > parseFloat(minLat) &&
+            p.postions.lat < parseFloat(maxLat) &&
+            p.postions.lng > parseFloat(minLng) &&
+            p.postions.lng < parseFloat(maxLng)
+        );
+        const pinsArray = [];
+        let pinArray = [];
+        inZone.sort(sortPin);
+        inZone.forEach((pinInZone, index) => {
+          if (index !== 0) {
+            const prevPin = inZone[index - 1];
+            if (
+              prevPin.postions.lat !== pinInZone.postions.lat ||
+              prevPin.postions.lng !== pinInZone.postions.lng
+            ) {
+              pinsArray.push(pinArray);
+              pinArray = [];
+            }
+          }
+          pinArray.push(pinInZone);
+          if(index === inZone.length-1){
+            pinsArray.push(pinArray); 
+          }
+        });
+        console.log(pinsArray)
+        return res.status(200).json(pinsArray);
       }
     });
   } else {
@@ -22,9 +66,35 @@ exports.types = (req, res) => {
       if (err) {
         return res.status(500).json({ message: err.message });
       } else {
-        return res.status(200).json(pin);
+        const inZone = pin.filter(
+          (p) =>
+            p.postions.lat > parseFloat(minLat) &&
+            p.postions.lat < parseFloat(maxLat) &&
+            p.postions.lng > parseFloat(minLng) &&
+            p.postions.lng < parseFloat(maxLng)
+        );
+        const pinsArray = [];
+        let pinArray = [];
+        inZone.sort(sortPin);
+        inZone.forEach((pinInZone, index) => {
+          if (index !== 0) {
+            const prevPin = inZone[index - 1];
+            if (
+              prevPin.postions.lat !== pinInZone.postions.lat ||
+              prevPin.postions.lng !== pinInZone.postions.lng
+            ) {
+              pinsArray.push(pinArray);
+              pinArray = [];
+            }
+          }
+          pinArray.push(pinInZone);
+          if(index === inZone.length-1){
+            pinsArray.push(pinArray); 
+          }
+        });
+        console.log(pinsArray)
+        return res.status(200).json(pinsArray);
       }
     });
   }
 };
-
